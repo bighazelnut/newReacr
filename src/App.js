de48e376233a32eec2215af5ebee2cab86c75da5
bighazelnut <167824134+bighazelnut@users.noreply.github.com>
2024-04-23 01:44:25 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { MainHui } from './MainHui';
+
+function App() {
+  return (
+    <div className="App">
+      <MainHui
+        id={435}
+        name='Саня Орех'
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainHui.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export const MainHui = (props) => {
+
+    const {
+        id,
+        name
+    } = props;
+
+    const [valueForInput, setValueForInput] = useState('');
+    const [viewData, setViewData] = useState('');
+
+    return (
+        <div>
+            <input 
+                placeholder="Введи что-то"
+                value={valueForInput}
+                onChange={(e) => setValueForInput(e.target.value)}
+            />
+            <p>{viewData}</p>
+            <button
+                onClick={() => setViewData(valueForInput)}
+            >
+                Показать что ввел
+            </button>
+            <button
+                onClick={() => setViewData("")}
+            >
+                Стереть данные
+            </button>
+            <h1>Name: {name}</h1>
+            <p>ID: {id}</p>
+        </div>
+    )
+}
